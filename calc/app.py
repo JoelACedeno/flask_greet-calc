c5145019c00d9f6ea95a47d3b5a4047d780a06db
JoelACedeno <123585946+JoelACedeno@users.noreply.github.com>
2023-04-18 12:17:36 -0400

    Add files via upload

---
+++ calc/app.py
@@ -0,0 +1,56 @@
+# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route('/add')
+def do_add():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = add(a, b)
+
+    return str(result)
+
+@app.route('/sub')
+def do_sub():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = sub(a,b)
+
+    return str(result)
+
+@app.route('/mult')
+def do_mult():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = mult(a, b)
+
+    return str(result)
+
+@app.route('/div')
+def do_div():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = div(a, b)
+
+    return str(result)
+
+##################################################
+# FURTHER STUDY
+
+operators = {
+    "add": add,
+    "sub": sub,
+    "div": div,
+    "mult": mult
+}
+
+@app.route('/math/<oper>')
+def do_math(op):
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+
+    result = operators[op](a, b)
+
+    return str(result)
